b5da98de392a464640457b68e74bdf20c52e5436
gogi999 <g.petrovic9@gmail.com>
2023-08-23 11:41:48 +0200

    Started to refactor code with RTK

--- src/features/accounts/AccountOperations.js
+++ src/features/accounts/AccountOperations.js
@@ -27,12 +27,14 @@ function AccountOperations() {
     isLoading
   } = useSelector((store) => store.account);
 
+  console.log(balance);
+
   function handleDeposit() {
     if (!depositAmount) return;
 
-    dispatch(deposit(depositAmount, currency));
+    dispatch(deposit(depositAmount));
     setDepositAmount('');
-    setCurrency('');
+    setCurrency('USD');
   }
 
   function handleWithdrawal() {
