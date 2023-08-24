bcb9cf06307a15c73518968c9efafe73070c369d
gogi999 <g.petrovic9@gmail.com>
2023-08-24 10:07:42 +0200

    Added thunk

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -13,6 +13,7 @@ const accountSlice = createSlice({
     reducers: {
         deposit(state, action) {
             state.balance += action.payload;
+            state.isLoading = false;
         },
         withdraw(state, action) {
             state.balance -= action.payload;
@@ -32,19 +33,41 @@ const accountSlice = createSlice({
                 state.balance += action.payload.amount;
             },
         },
-        payLoan(state, action) {
+        payLoan(state) {
             state.balance -= state.loan;
             state.loan = 0;
             state.loanPurpose = '';
+        },
+        convertingCurrency(state) {
+            state.isLoading = true;
         }
     }
 });
 
-console.log(accountSlice);
+export const { payLoan, requestLoan, withdraw } = accountSlice.actions;
 
-export const { deposit, payLoan, requestLoan, withdraw }
-= accountSlice.actions;
+export function deposit(amount, currency) {
+    if (currency === 'USD') return { 
+        type: 'account/deposit', 
+        payload: amount 
+    };
 
-console.log(requestLoan(1000, 'Buy a car'));
+    return async function(dispatch, getState) {
+        dispatch({ type: 'account/convertingCurrency' });
+
+        // API call 
+        const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+        );
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        // dispatch action
+        dispatch({ 
+            type: 'account/deposit', 
+            payload: converted 
+        });
+    }
+}
 
 export default accountSlice.reducer;
